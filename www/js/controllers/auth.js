3ae47332cea3663ed9a09176528be0ac56b8677a
Rhiannon Argote <rhiannon.argote@gmail.com>
2016-08-11 15:41:36 +0100

    Tutorial LoverApp Section 1-3 OAuth not working

---
+++ www/js/controllers/auth.js
@@ -0,0 +1,19 @@
+'use strict';
+
+app.controller('AuthCtrl', function(Auth) {
+
+  var auth = this;
+
+  auth.login = function() {
+    console.log('Login clicked');
+    
+    return Auth.login().then(function(user) {
+      console.log(user.facebook);
+    });
+    
+  };
+
+  auth.logout = function() {
+    Auth.logout();
+  };
+});
---
+++ www/js/services/auth.js
@@ -0,0 +1,33 @@
+'use strict';
+
+app.factory('Auth', function(FURL, $firebaseAuth, $firebaseObject, $state) {
+
+  var ref = new Firebase(FURL);
+  var auth = $firebaseAuth(ref);
+
+  var Auth = {
+    login: function() {
+      return auth.$authWithOAuthPopup('facebook', {
+        remember: "sessionOnly",
+        scope: "public_profile, email, user_location, user_birthday, user_photos, user_about_me"
+      });
+    },
+
+    logout: function() {
+      return auth.$unauth();
+    }   
+
+  };
+
+  auth.$onAuth(function(authData) {
+    if(authData) {
+      console.log('Logged in!');
+    } else {
+      $state.go('login');
+      console.log('You need to login.');
+    }
+  });
+
+  return Auth;
+
+});
